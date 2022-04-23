9da2d313daec61bb4ac6b9ceaa5e46a728bc3c9f
Yana Mironyak <yanika.mironyak@gmail.com>
2022-04-23 22:30:24 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elements = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+
+  return itemEl;
+});
+
+// console.log(elements);
+
+const listEl = document.createElement('ul');
+listEl.classList.add('ingredients');
+console.log(listEl);
+listEl.append(...elements);
